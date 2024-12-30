9a07703f4672c7bc2309b5ba6d8b03cc5aa588fe
Void <112571254+TheVoidMonarch@users.noreply.github.com>
2024-12-30 11:20:27 +0800

    Update ft_printf.c

--- 42-Core/2.1.printf/ft_printf.c
+++ 42-Core/2.1.printf/ft_printf.c
@@ -1,55 +1,96 @@
 #include "ft_printf.h"
+#include <stdarg.h>
+#include <unistd.h>
 
 int ft_putchar(int c)
 {
-	write(1, &c, 1);
-	return (1);
+    write(1, &c, 1);
+    return (1);
+}
+
+int ft_putstr(char *str)
+{
+    int i = 0;
+
+    while (str[i])
+    {
+        ft_putchar(str[i]);
+        i++;
+    }
+    return (i);
+}
+
+int ft_putnbr(int n)
+{
+    char buffer[11];
+    int i = 0;
+
+    if (n == 0)
+    {
+        ft_putchar('0');
+        return (1);
+    }
+    if (n < 0)
+    {
+        ft_putchar('-');
+        n = -n;
+        i++;
+    }
+    while (n > 0)
+    {
+        buffer[i++] = (n % 10) + '0';
+        n /= 10;
+    }
+    buffer[i] = '\0';
+    // Reverse the buffer
+    for (int j = 0; j < i / 2; j++)
+    {
+        char temp = buffer[j];
+        buffer[j] = buffer[i - j - 1];
+        buffer[i - j - 1] = temp;
+    }
+    ft_putstr(buffer);
+    return (i);
 }
 
 int ft_format(va_list args, char form)
 {
-	int count;
+    int count = 0;
 
-	count = 0;
-	if (form == 'c')
-		count += ft_putchar(va_args(args, int));
-	else if (form == 's')
-		count += ft_putstr(va_args(args, char *));
-	else if (form == 'd' || form == 'i')
-		count += ft_putnbr(va_args(args, int));
-	else if (form == 'u')
-		count += ft_putunsigned(va_args(args, unsigned int));
-	else if (form == 'p')
-		count += ft_putpointer(va_args(args, unsigned long long));
-	else if (form == 'x' || form == 'X')
-		count += ft_puthex(va_args(args, unsigned int), form);
-	else if (form == '%')
-		count += ft_putpercent();
-	return (count);
+    if (form == 'c')
+        count += ft_putchar(va_arg(args, int));
+    else if (form == 's')
+        count += ft_putstr(va_arg(args, char *));
+    else if (form == 'd' || form == 'i')
+        count += ft_putnbr(va_arg(args, int));
+    // Add other format specifiers (u, p, x, X, %) as needed
+    else if (form == '%')
+        count += ft_putchar('%');
+    else
+        count += ft_putchar(form); // Handle invalid format specifier by printing it
+    return (count);
 }
 
 int ft_printf(const char *str, ...)
 {
-	int i;
-	va_list args;
-	int count;
+    int i = 0;
+    va_list args;
+    int count = 0;
 
-	i = 0;
-	count = 0;
-	va_start(args, str);
-	while (str[i])
-	{
-		if str[i] == '%';
-		{
-			count += ft_format(args, str[i + 1]);
-			i++;
-		}
-		else
-		{
-			count += ft_putchar(str[i]);
-		}
-		i++;
-	}
-	va_end(args);
-	return (count);
+    va_start(args, str);
+    while (str[i])
+    {
+        if (str[i] == '%')
+        {
+            count += ft_format(args, str[i + 1]);
+            i++;
+        }
+        else
+        {
+            count += ft_putchar(str[i]);
+        }
+        i++;
+    }
+    va_end(args);
+    return (count);
 }
